f1fc3817572c97cd6a07e8a93100c9f587d7162d
micaldas <micaldas@mailfence.com>
2021-06-18 18:35:21 +0100

    python

--- gitpush.py
+++ gitpush.py
@@ -1,20 +1,10 @@
-#!/usr/bin/env python3
-
-""" Script to automate the push process in Git """
-from colr import color
-from sultan.api import Sultan
+import subprocess
 
 
 def push():
-    """ User will define a title for the commit and Sultan will add, commit and push """
-
-    title = input(color('What is the title of the commit?', fore='#feb729'))
-
-    with Sultan.load() as s:
-        s.git('add .').run()
-        s.git('commit -m ' + title).run()
-        s.git('push origin main').run()
-        s.git('push origin_gogs main').run()
+    title = input('Choose a title for the commit ')
+    subprocess.run(['git add .'], shell=True)
+    subprocess.run(['git commit -m ' + title], shell=True)
 
 
 if __name__ == '__main__':
