bec479fec1ea758914c7fdfbeda7789269bc9326
micaldas <micaldas@mailfence.com>
2021-06-18 18:37:38 +0100

    python

--- gitpush.py
+++ gitpush.py
@@ -5,6 +5,8 @@ def push():
     title = input('Choose a title for the commit ')
     subprocess.run(['git add .'], shell=True)
     subprocess.run(['git commit -m ' + title], shell=True)
+    subprocess.run(['git push origin main'], shell=True)
+    subprocess.run(['git push origin_gogs main'], shell=True)
 
 
 if __name__ == '__main__':
