3c4b49086002ddeb56854725d1534c0475a92814
micaldas <micaldas@mailfence.com>
2021-09-29 18:30:43 +0100

    Update

--- player.py
+++ player.py
@@ -9,7 +9,7 @@ import click
 
 
 def player():
-    """ The program should present the content of the 'music' folder, create a prompt for the user to choose from, and reproduce the files
+    """The program should present the content of the 'music' folder, create a prompt for the user to choose from, and reproduce the files
     with a timer."""
     # https://stackoverflow.com/questions/5817209/browse-files-and-subfolders-in-python
     for i in os.scandir("/home/mic/music"):
@@ -17,7 +17,8 @@ def player():
             print(i.path)
         elif i.is_dir():
             print(i.path)
-    print(r"""
+    print(
+        r"""
       ___           ___           ___                       ___                    ___                       ___                       ___           ___
      /\  \         /\  \         /\__\                     /\__\                  /\  \                     /\  \                     /\__\         /\  \
     |::\  \        \:\  \       /:/ _/_       ___         /:/  /                 /::\  \                   /::\  \         ___       /:/ _/_       /::\  \
@@ -29,26 +30,27 @@ def player():
    \:\  \        \:\/:/  /     \/_/:/  /       \::/  /   \:\/:/  /              \:\  \      \:\/:/  /     \:\  \      ~~~~\:\  \    \:\/:/  /     \:\~~\
     \:\__\        \::/  /        /:/  /        /:/  /     \::/  /                \:\__\      \::/  /       \:\__\          \:\__\    \::/  /       \:\__\
      \/__/         \/__/         \/__/         \/__/       \/__/                  \/__/       \/__/         \/__/           \/__/     \/__/         \/__/
-""")
-    print('\n')
-    escolha = str(input(click.style(' » What do you want to hear? :~ ', fg='green', bold=True)))
-    print('\n')
+"""
+    )
+    print("\n")
+    escolha = str(input(click.style(" » What do you want to hear? :~ ", fg="green", bold=True)))
+    print("\n")
     album = []
     for i in os.scandir(escolha):
         if i.is_file():
             album.append(i.path)
     for song in album:
         musicas = os.path.basename(os.path.normpath(song))
-        print(click.style(musicas, fg='bright_white', bold=True))
+        print(click.style(musicas, fg="bright_white", bold=True))
         sound = AudioSegment.from_file(song)
         threading.get_ident()
         t = threading.Thread(target=play, args=(sound,))
         t.start()
         dur = sound.duration_seconds
-        for sec in tqdm(range(int(dur)), bar_format='{desc}: {percentage:.0f}%|{bar} | {elapsed}<{remaining}'):
+        for sec in tqdm(range(int(dur)), bar_format="{desc}: {percentage:.0f}%|{bar} | {elapsed}<{remaining}"):
             sleep(1)
-        print('\n')
+        print("\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     player()
