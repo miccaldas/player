35db1b1e729c635d89c280db4c8d940527dc93a9
micaldas <micaldas@mailfence.com>
2023-07-16 16:41:10 +0100

    16-07-2023 commit

--- player/main.py
+++ player/main.py
@@ -1,23 +1,19 @@
 """ Here we call the commands functions from the Commands class, and present them to the user as a multiple choice questionart """
 from __future__ import unicode_literals
 import sys
-from commands import Commands
 from player import player
 import questionary
-from virt import find, mata, suspend, resume, get_vk, is_combination_pressed, on_press, on_release
+from commands import find, mata, suspend, resume, get_vk, is_combination_pressed, on_press, on_release
 
 resposta = questionary.select("What do you want do to? ", choices=["Start the Player", "Stop the Player", "Suspend the Player", "Resume the Player", "Exit"]).ask()
 
 if resposta == "Start the Player":
     player()
 if resposta == "Stop the Player":
-    com = Commands()  # It is always needed to instantiate the class. Only then can you access the methods
-    com.mata()
+    mata()
 if resposta == "Suspend the Player":
-    com = Commands()
-    com.suspend()
+    suspend()
 if resposta == "Resume the Player":
-    com = Commands()
-    com.resume()
+    resume()
 if resposta == "Exit":
     sys.exit()
